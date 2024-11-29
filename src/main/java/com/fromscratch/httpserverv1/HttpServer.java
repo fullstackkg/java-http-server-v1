9aa03faa6019a37e9e7ae8d66474e887728e335b
fullstackkg <khalilhgayle@gmail.com>
2024-11-29 00:48:59 -0500

    Initial commit

---
+++ src/main/java/com/fromscratch/httpserverv1/HttpServer.java
@@ -0,0 +1,11 @@
+package com.fromscratch.httpserverv1;
+
+/*
+This class is responsible for getting the server started,
+everything runs through here.
+*/
+public class HttpServer {
+    public static void main(String[] args) {
+        System.out.println("Server is running...");
+    }
+}
